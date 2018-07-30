4912a4bfd2ba13892b000710a0f5749b33c28071
Michael <michael@codetropolis.com>
2018-07-30 18:16:23 -0500

    Fiancials entry.component - detect if cost exist.  If cost does not exist, show input form for cost.

--- src/app/financials/entry/entry.component.ts
+++ src/app/financials/entry/entry.component.ts
@@ -21,6 +21,7 @@ export class EntryComponent implements OnInit {
   public currentFinancialDoc: any;
   public category: string;
   public formGroup: FormGroup;
+  // public costExists: boolean[] = [];
   public costExists: boolean;
 
   constructor(private financialService: FinancialsService, private dataService: DataService, private fb: FormBuilder) { }
@@ -34,7 +35,7 @@ export class EntryComponent implements OnInit {
         this.category = x;
         // Set up object keys (based on current category) to send to DB.
         if (this.category) {
-          console.log(`category: ${this.category}`);
+         // console.log(`category: ${this.category}`);
           this.costKey = this.category + 'Cost';
           this.paymentKey = this.category + 'Payment';
           this.deductionKey = this.category + 'Deduction'
@@ -51,11 +52,13 @@ export class EntryComponent implements OnInit {
     this.currentFinancialDoc.ref.get().then(
       snapshot => {
         if (snapshot.data()[costKey]) {
-          console.log(`Cost for ${costKey}: ${snapshot.data()[costKey]}`);
+          //console.log(`Cost for ${costKey}: ${snapshot.data()[costKey]}`);
           this.costExists = true;
+          //console.log(`costExists: ${this.costExists[this.category]}`);
         } else {
-          console.log(`Cost for ${costKey} does not exist`);
+          //console.log(`Cost for ${costKey} does not exist`);
           this.costExists = false;
+          // console.log(`costExists: ${this.costExists[this.category]}`);
         }
         this.setupFormGroup(this.category); // Do this only after cost state determined.
       }
@@ -63,29 +66,38 @@ export class EntryComponent implements OnInit {
   }
 
   private setupFormGroup(cat: string) {
+
     if (!this.costExists) { // Cost for category does not exist so form provides cost control based on category.
       //console.log(`In setupFormGroup - costExist: ${this.costExists}`)
       this.formGroup = this.fb.group({
         [this.costKey]: ['', Validators.required],
       });
-    } else { // Cost exist at this point. Now set up form for a payment field for tuition and payment and deduction fields for other categories.
-      if (cat === 'tuition') {
-        this.formGroup = this.fb.group({
-         [this.paymentKey]: ['', Validators.required], // For tuition, payments deduct from starting cost/balance
-        });
-      } else {
-        this.formGroup = this.fb.group({
-          [this.paymentKey]: [''], // For all other categories, payment will add to starting amount
-          [this.deductionKey]: ['']
-        });
-      }
+    }else{
+      console.log('setup payment and deduction form controls');
     }
+
+    // if (!this.costExists) { // Cost for category does not exist so form provides cost control based on category.
+    //   //console.log(`In setupFormGroup - costExist: ${this.costExists}`)
+    //   this.formGroup = this.fb.group({
+    //     [this.costKey]: ['', Validators.required],
+    //   });
+    // } else { // Cost exist at this point. Now set up form for a payment field for tuition and payment and deduction fields for other categories.
+    //   if (cat === 'tuition') {
+    //     this.formGroup = this.fb.group({
+    //      [this.paymentKey]: ['', Validators.required], // For tuition, payments deduct from starting cost/balance
+    //     });
+    //   } else {
+    //     this.formGroup = this.fb.group({
+    //       [this.paymentKey]: [''], // For all other categories, payment will add to starting amount
+    //       [this.deductionKey]: ['']
+    //     });
+    //   }
+    // }
 
   }
 
   async submitHandler(formDirective) {
     const formValue = this.formGroup.value;
-
     try {
       // if only use formValue instead of formValue[this.cost], then object will be submitted to DB
       await this.currentFinancialDoc.set({ [this.costKey]: formValue[this.costKey] }, { merge: true })
