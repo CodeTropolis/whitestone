49c8bb25a609d89a6af85704fa411881f79b57c1
Michael <michael@codetropolis.com>
2018-09-12 16:24:42 -0500

    Changed environment.prod.ts to use dummy db.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,24 @@
+// export const environment = {
+//   production: true,
+//   firebase: {
+//     apiKey: "AIzaSyB5edtPdugpbEgUR0BltaWCa1FWdrFh3zc",
+//     authDomain: "whitestone-5433a.firebaseapp.com",
+//     databaseURL: "https://whitestone-5433a.firebaseio.com",
+//     projectId: "whitestone-5433a",
+//     storageBucket: "",
+//     messagingSenderId: "512131240408"
+//   }
+// };
+
+// Dev DB:
 export const environment = {
-  production: true,
+  production: false,
   firebase: {
-    apiKey: "AIzaSyB5edtPdugpbEgUR0BltaWCa1FWdrFh3zc",
-    authDomain: "whitestone-5433a.firebaseapp.com",
-    databaseURL: "https://whitestone-5433a.firebaseio.com",
-    projectId: "whitestone-5433a",
-    storageBucket: "",
-    messagingSenderId: "512131240408"
+    apiKey: "AIzaSyBgRe4SH2Jge-Yy2Mn-JOoCk8j4Qj7YhhU",
+    authDomain: "projx-e0882.firebaseapp.com",
+    databaseURL: "https://projx-e0882.firebaseio.com",
+    projectId: "projx-e0882",
+    storageBucket: "projx-e0882.appspot.com",
+    messagingSenderId: "740695221633"
   }
 };
