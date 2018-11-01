2f63eb07cba3e6c20b53fc05175a289ecdd9e6ad
Michael <michael@codetropolis.com>
2018-11-01 17:13:19 -0500

    Commi.t

--- src/app/core/services/data.service.ts
+++ src/app/core/services/data.service.ts
@@ -42,38 +42,38 @@ export class DataService {
   }
 
   // Creates the base doc (as an observable) for all the selected student's financials
-  public setFinancialDoc(child) {
-    this.currentChild$.next(child); // ToDo: For category-select.component - get child's name from financials document.
-    this.currentFinancialDoc$ = this.firebaseService.financialsCollection.doc(child.id).snapshotChanges()
-      .pipe(
-        tap((doc => {
-         // console.log(`pipe(tap.. : ${doc.payload.ref.id}`); // tap with log alerts us that there is a subscriber
-          doc.payload.ref.get().then(snapshot => {
+  // public setFinancialDoc(child) {
+  //   this.currentChild$.next(child); // ToDo: For category-select.component - get child's name from financials document.
+  //   this.currentFinancialDoc$ = this.firebaseService.financialsCollection.doc(child.id).snapshotChanges()
+  //     .pipe(
+  //       tap((doc => {
+  //        // console.log(`pipe(tap.. : ${doc.payload.ref.id}`); // tap with log alerts us that there is a subscriber
+  //         doc.payload.ref.get().then(snapshot => {
 
-            // Pass the email to the financial document in order to secure reads to match user email.  
-            // Outside of if (!snapshot.exists) because this needs to be done for future as well as existing financial docs. 
-
-            // Only admin user can write per Firestore rule and financial doc should only be created if user admin role is true.
-          
-            if (this.authService.user['roles'].admin){
-              doc.payload.ref.set({recordId: this.currentRecord.realId}); 
-              doc.payload.ref.set({childFname: child.fname, childLname: child.lname}); 
-              if(this.currentRecord.fatherEmail){
-                doc.payload.ref.set({ fatherEmail: this.currentRecord.fatherEmail});
-                //console.log('write') // Since this is within snapshotChanges() this will write on every doc change.
-              }
-              if(this.currentRecord.motherEmail){
-                doc.payload.ref.set({ motherEmail: this.currentRecord.motherEmail});  
-              }
-              if (!snapshot.exists) {
-                doc.payload.ref.set({ dateCreated: new Date });
-              }
-            }
-          });
-        }),
-        ),
-        shareReplay(1), // Give all subscribers a cached version of the doc which I'm thinking should be refreshed on snapshotChanges.
-      );
-  }
+  //           // Pass the email to the financial document in order to secure reads to match user email.  
+  //           // Outside of if (!snapshot.exists) because this needs to be done for future as well as existing financial docs. 
+  //           // NOTE because of being outside of if (!snapshot.exists) and snapshotChanges() , this causes infinite loop in subscription to currentFinancialDoc$
+    
+  //           // Only admin user can write per Firestore rule and financial doc should only be created if user admin role is true.
+  //           if (this.authService.user['roles'].admin){
+  //             // doc.payload.ref.set({recordId: this.currentRecord.realId}); 
+  //             // doc.payload.ref.set({childFname: child.fname, childLname: child.lname}); 
+  //             // if(this.currentRecord.fatherEmail){
+  //             //   doc.payload.ref.set({ fatherEmail: this.currentRecord.fatherEmail});
+  //             //   //console.log('write') // Since this is within snapshotChanges() this will write on every doc change.
+  //             // }
+  //             // if(this.currentRecord.motherEmail){
+  //             //   doc.payload.ref.set({ motherEmail: this.currentRecord.motherEmail});  
+  //             // }
+  //             if (!snapshot.exists) {
+  //               doc.payload.ref.set({ dateCreated: new Date });
+  //             }
+  //           }
+  //         });
+  //       }),
+  //       ),
+  //       shareReplay(1), // Give all subscribers a cached version of the doc which I'm thinking should be refreshed on snapshotChanges.
+  //     );
+  // }
 
 }
