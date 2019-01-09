4e506dccb4f58ac71dbac391daf10b33b0e67eae
Michael <michael@codetropolis.com>
2019-01-09 16:23:04 -0600

    Header component mobile responsive and @input for UI element.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -12,6 +12,7 @@ export class HeaderComponent implements OnInit {
   @Input() title = 'Header...';
   @Input() link;
   @Input() linkName;
+  @Input() showUserEmail = false;
 
   public currentUser$: Observable<any>;
 
