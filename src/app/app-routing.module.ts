aa57ec72d7ad9d1f5f696af79f00b47f3b6c4beb
Michael <michael@codetropolis.com>
2019-02-11 11:00:57 -0600

    Added Grades path to app-routing.module and added link to grades route to Grades button in record-list.componnet.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,19 +2,19 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { AuthGuard } from './core/guards/auth.guard';
-//import { RecordResolverService } from './core/services/record-resolver.service';
 
 const routes: Routes = [
   {path: '', component: LoginComponent},
   {path: 'login', component: LoginComponent},
   {path: 'record-list', loadChildren: './record/record.module#RecordModule', 
   canLoad:[AuthGuard],
- // canActivate:[AuthGuard]
-  //resolve: {records: RecordResolverService}
   },
   {path: 'financials', loadChildren: './financials/financials.module#FinancialsModule', 
   canLoad:[AuthGuard],
   },
+  {path: 'grades', loadChildren: './grades/grades.module#GradesModule', 
+  canLoad:[AuthGuard],
+  }
 ];
 
 @NgModule({
