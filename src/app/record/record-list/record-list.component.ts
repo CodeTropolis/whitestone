d098faac6d97ec720eb13e143db0e0c4719c4af9
Michael <michael@codetropolis.com>
2019-02-11 20:59:51 -0600

    Removed userIsAdmin and userIsSubscriber bools from record-list.

--- src/app/record/record-list/record-list.component.ts
+++ src/app/record/record-list/record-list.component.ts
@@ -21,18 +21,15 @@ import { switchMap } from 'rxjs/operators';
 })
 export class RecordListComponent implements OnInit {
 
+  public user: any;
+  public records$: Observable<any[]>;
   public isUpdating: boolean;
   public ds: MatTableDataSource<any>;
   public displayedColumns;
   public showChildren: boolean[] = [];
   public isDeleting: boolean[] = [];
   public showForm: boolean;
-
   public loading: boolean = true;
-
-  // public user: User;
-  public userIsAdmin: boolean = false;  // for view
-  public userIsSubcriber: boolean = false;
   public recordMatch: boolean;
 
   // For modal
@@ -44,10 +41,6 @@ export class RecordListComponent implements OnInit {
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
-
-  public records$: Observable<any[]>;
-
-  public  user: any;
 
   constructor(
     private fs: FirebaseService,
