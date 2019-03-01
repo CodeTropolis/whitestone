07e74dccc23a1df478b0d971b9ba21e54225d0e9
Michael <michael@codetropolis.com>
2019-03-01 14:38:23 -0600

    Code clean up, correct spelling erros.

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,23 +1,17 @@
-import { Injectable } from '@angular/core';
-
-import { BehaviorSubject, Observable, of } from 'rxjs';
-import {  switchMap } from 'rxjs/operators';
-
-// import { AngularFireAuth } from 'angularfire2/auth';
-// import { AngularFirestore, AngularFirestoreDocument } from 'angularfire2/firestore';
-
-
-import { AngularFireAuth } from '@angular/fire/auth';
-import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
-
-import { User } from '../user';
-
+import { Injectable } from "@angular/core";
+import { BehaviorSubject, Observable, of } from "rxjs";
+import { switchMap } from "rxjs/operators";
+import { AngularFireAuth } from "@angular/fire/auth";
+import {
+  AngularFirestore,
+  AngularFirestoreDocument
+} from "@angular/fire/firestore";
+import { User } from "../user";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class AuthService {
-
   public user$: Observable<User>;
   public status$ = new BehaviorSubject<string>(null);
   public error$ = new BehaviorSubject<string>(null);
@@ -27,31 +21,31 @@ export class AuthService {
   public disableLoginOrCreateButton$ = new BehaviorSubject<boolean>(null);
 
   constructor(private afAuth: AngularFireAuth, private afs: AngularFirestore) {
-
     this.user$ = this.afAuth.authState.pipe(
       switchMap(user => {
         if (user) {
-          return this.afs.doc<User>(`users/${user.uid}`).valueChanges()
+          return this.afs.doc<User>(`users/${user.uid}`).valueChanges();
         } else {
-          return of(null)
+          return of(null);
         }
       })
-    )
-
+    );
   }
 
   public emailLogin(e, p) {
     this.disableLoginOrCreateButton$.next(true);
-    this.status$.next('');
+    this.status$.next("");
     const promise = this.afAuth.auth.signInWithEmailAndPassword(e, p);
     promise.then(credential => {
       this.disableLoginOrCreateButton$.next(false);
       if (this.afAuth.auth.currentUser.emailVerified) {
         this.setUserData(credential.user);
       } else {
-        this.error$.next('Email not verfied. If you have already signed up, please visit your inbox and verify your email.  Once verified, you may return to the app and login.');
+        this.error$.next(
+          "Email not verified. If you have already signed up, please visit your inbox and verify your email.  Once verified, you may return to the app and login."
+        );
       }
-    })
+    });
     promise.catch(err => {
       console.log(err);
       this.error$.next(err.message);
@@ -62,44 +56,46 @@ export class AuthService {
     this.disableLoginOrCreateButton$.next(true);
     const promise = this.afAuth.auth.createUserWithEmailAndPassword(e, p);
     promise.then(success => {
-      this.error$.next('');
+      this.error$.next("");
       let user: any = this.afAuth.auth.currentUser;
-      user.sendEmailVerification().then(_ => {
-        this.creatingAccount$.next(false);
-        this.status$.next('Thank you. A verification email has been sent to your email address. It is possible that the email was sent to your spam folder. Once verified, you may return to the app and login.');
-      }
-      ).catch(
-        (err) => {
+      user.sendEmailVerification()
+        .then(_ => {
+          this.creatingAccount$.next(false);
+          this.status$.next(
+            "Thank you. A verification email has been sent to your email address. It is possible that the email was sent to your spam folder. Once verified, you may return to the app and login."
+          );
+        })
+        .catch(err => {
           console.log(err);
-          this.error$.next(err)
-        }
-      )
+          this.error$.next(err);
+        });
       this.disableLoginOrCreateButton$.next(false);
-    })
+    });
     promise.catch(err => {
       console.log(err);
       this.error$.next(err);
-      this.creatingAccount$.next(false); // Perhaps the user tried to create an account when it already exists so switch back to login or create account UI.  
+      this.creatingAccount$.next(false); // Perhaps the user tried to create an account when it already exists so switch back to login or create account UI.
       this.disableLoginOrCreateButton$.next(false);
     });
-
   }
 
   public resetPassword(email: string) {
-    return this.afAuth.auth.sendPasswordResetEmail(email)
-      .then(() =>{ 
+    return this.afAuth.auth
+      .sendPasswordResetEmail(email)
+      .then(() => {
         console.log("email sent");
-        this.status$.next(`Password reset instructions have been sent to ${email}.`);
+        this.status$.next(
+          `Password reset instructions have been sent to ${email}.`
+        );
       })
-      .catch((error) => console.log(error))
+      .catch(error => console.log(error));
   }
 
   public logOut() {
-    this.afAuth.auth.signOut()
-    .then(() => {
-      // Prevent 'Missing or insufficient permissions...' errors upon logout. 
+    this.afAuth.auth.signOut().then(() => {
+      // Prevent 'Missing or insufficient permissions...' errors upon logout.
       // See https://medium.com/@dalenguyen/handle-missing-or-insufficient-permissions-firestore-error-on-angular-or-ionic-bf4edb7a7c68
-      window.location.reload(); 
+      window.location.reload();
     });
   }
 
@@ -108,29 +104,28 @@ export class AuthService {
   }
 
   private setUserData(user) {
+    const userRef: AngularFirestoreDocument<any> = this.afs.doc(
+      `users/${user.uid}`
+    );
 
-    const userRef: AngularFirestoreDocument<any> = this.afs.doc(`users/${user.uid}`);
-
-    // Every user should have the same custom data in regards to roles.  
+    // Every user should have the same custom data in regards to roles.
     // At this point, users are converted to admin users via Firebase console.
 
     const data: User = {
       uid: user.uid,
       email: user.email,
       roles: {
-        admin: false, 
+        admin: false,
         subscriber: true
       }
-    }
+    };
 
     // Only set user data if user doesn't exist.
     userRef.ref.get().then(doc => {
-      if(!doc.exists){
-        console.log('Setting user data.');
-        userRef.set(data, { merge: true })
+      if (!doc.exists) {
+        console.log("Setting user data.");
+        userRef.set(data, { merge: true });
       }
-    })
-    
+    });
   }
-
 }
--- src/app/financials/history/history.component.ts
+++ src/app/financials/history/history.component.ts
@@ -1,17 +1,16 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { FinancialsService } from '../financials.service';
-import { BehaviorSubject } from 'rxjs';
-import { MatSort, MatTableDataSource } from '@angular/material';
-import { AuthService } from '../../core/services/auth.service';
-import { delay } from 'rxjs/operators';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { FinancialsService } from "../financials.service";
+import { BehaviorSubject } from "rxjs";
+import { MatSort, MatTableDataSource } from "@angular/material";
+import { AuthService } from "../../core/services/auth.service";
+//import { delay } from "rxjs/operators";
 
 @Component({
-  selector: 'app-history',
-  templateUrl: './history.component.html',
-  styleUrls: ['./history.component.css']
+  selector: "app-history",
+  templateUrl: "./history.component.html",
+  styleUrls: ["./history.component.css"]
 })
 export class HistoryComponent implements OnInit {
-
   private subscriptions: any[] = [];
 
   public currentFinancialDoc: any;
@@ -24,125 +23,137 @@ export class HistoryComponent implements OnInit {
   @ViewChild(MatSort) sort: MatSort;
   public disableDelete: boolean[] = [];
   public user: any;
-  //public userIsReady: boolean;
- //public loading: boolean;
 
   private runningBalance: number;
   private updatedBalance: number;
   private chargesCollection: string;
   private paymentsCollection: string;
 
-  constructor(private financialsService: FinancialsService, private authService: AuthService) { }
+  constructor(
+    private financialsService: FinancialsService,
+    private authService: AuthService
+  ) {}
 
   ngOnInit() {
-
-    //this.loading = true;
-    //this.userIsReady = false;
-  
     this.subscriptions.push(
       this.authService.user$
-      //.pipe(delay(2000))
-      .subscribe(user =>{
-        if (user){
-          this.user = user; // For conditionals in view i.e. *ngIf="user['roles].admin"
-         // this.userIsReady = true; // So that in *ngIf="user['roles].admin" user doesn't end up undefined
-          if(this.user['roles'].admin){
-            this.tableColumns = ['amount', 'type', 'date', 'memo', 'delete'];
-           }else{
-            this.tableColumns = ['amount', 'type', 'date', 'memo'];
-           }
-        }else{
-          //this.userIsReady = false;
-        }
-      })
-  );
+        //.pipe(delay(2000))
+        .subscribe(user => {
+          if (user) {
+            this.user = user; // For conditionals in view i.e. *ngIf="user['roles].admin"
+            if (this.user["roles"].admin) {
+              this.tableColumns = ["amount", "type", "date", "memo", "delete"];
+            } else {
+              this.tableColumns = ["amount", "type", "date", "memo"];
+            }
+          }
+        })
+    );
 
     // currentFinancialDoc$ next'd by student-category.component
-  this.subscriptions.push(
-    this.financialsService.currentFinancialDoc$.subscribe(doc =>{ 
-      if(doc){
-        this.currentFinancialDoc = doc;
-        this.currentChild = this.currentFinancialDoc.data().childFirstName + ' ' + this.currentFinancialDoc.data().childLastName;
-      }
-    })
-  );
-
-  this.subscriptions.push(
-    this.financialsService.currentCategory$.subscribe(cat => {
-      if(cat){
-        this.currentCategory = cat.val;
-        this.runningBalanceKey = cat.key + 'Balance'; // ensure keys always match other components.  ToDo: Single source of truth.
-        this.chargesCollection = cat.key + 'Charges';
-        this.paymentsCollection = cat.key + 'Payments';
-
-        if (this.currentFinancialDoc && this.chargesCollection && this.paymentsCollection){
-          this.setupHistory();
-        }else {
-          console.log('Issue with setting currentFinancialDoc, and/or payments/charges collection strings');
+    this.subscriptions.push(
+      this.financialsService.currentFinancialDoc$.subscribe(doc => {
+        if (doc) {
+          this.currentFinancialDoc = doc;
+          this.currentChild =
+            this.currentFinancialDoc.data().childFirstName +
+            " " +
+            this.currentFinancialDoc.data().childLastName;
         }
-      }
-    })
-  );
+      })
+    );
 
-  // Get the running balance which is next'd by entry.component. 
-  //  Trying to get it via this.currentFinancialDoc.data()[this.runningBalanceKey] sometimes returns as undefined
-    this.financialsService.runningBalanceForCurrentCategory$.subscribe(runningBalance => {
-      //console.log(runningBalance);
-      this.runningBalance = runningBalance;
-    });
-    
+    this.subscriptions.push(
+      this.financialsService.currentCategory$.subscribe(cat => {
+        if (cat) {
+          this.currentCategory = cat.val;
+          this.runningBalanceKey = cat.key + "Balance"; // ensure keys always match other components.  ToDo: Single source of truth.
+          this.chargesCollection = cat.key + "Charges";
+          this.paymentsCollection = cat.key + "Payments";
+
+          if (
+            this.currentFinancialDoc &&
+            this.chargesCollection &&
+            this.paymentsCollection
+          ) {
+            this.setupHistory();
+          } else {
+            console.log(
+              "Issue with setting currentFinancialDoc, and/or payments/charges collection strings"
+            );
+          }
+        }
+      })
+    );
+
+    // Get the running balance which is next'd by entry.component.
+    //  Trying to get it via this.currentFinancialDoc.data()[this.runningBalanceKey] sometimes returns as undefined
+    this.financialsService.runningBalanceForCurrentCategory$.subscribe(
+      runningBalance => {
+        //console.log(runningBalance);
+        this.runningBalance = runningBalance;
+      }
+    );
   } // end init()
 
-  private setupHistory(){
-
-    //console.log('setupHIstory()')
-
+  private setupHistory() {
     this.subscriptions.push(
       // Moving the subscription here may have fixed history sorting.
       // Unsure at this point because sorting issue seemed intermittent.
       this.financialsService.transactions$
-     // .pipe(delay(2000))
-      .subscribe(data => {
-    
-      // if(data) prevents 'Cannot read property 'slice' of null' error upon clicking 'View History'. 
-      // Reason: data property in subscription null for a moment until populated
-      if(data){ 
-          this.tableData = new MatTableDataSource(data);
-          this.tableData.sort = this.sort;
-          //this.loading = false;
-        }
-      })
-    )
+        // .pipe(delay(2000))
+        .subscribe(data => {
+          // if(data) prevents 'Cannot read property 'slice' of null' error upon clicking 'View History'.
+          // Reason: data property in subscription null for a moment until populated
+          if (data) {
+            this.tableData = new MatTableDataSource(data);
+            this.tableData.sort = this.sort;
+            //this.loading = false;
+          }
+        })
+    );
 
-    this.financialsService.transactions = []; //  to prevent dup entries. 
+    this.financialsService.transactions = []; //  to prevent dup entries.
     this.financialsService.transactions$.next(null);
-    this.financialsService.getTransactions(this.currentFinancialDoc, this.paymentsCollection);
-    this.financialsService.getTransactions(this.currentFinancialDoc, this.chargesCollection);
-
+    this.financialsService.getTransactions(
+      this.currentFinancialDoc,
+      this.paymentsCollection
+    );
+    this.financialsService.getTransactions(
+      this.currentFinancialDoc,
+      this.chargesCollection
+    );
   }
 
   public deleteTransaction(id: string, type: string, amount: number) {
-    
     this.disableDelete[id] = true; // Prevent user from entering delete multiple times for a row.
 
-    type === 'Payment' ? this.updatedBalance = (this.runningBalance + amount) : this.updatedBalance = (this.runningBalance - amount);
+    type === "Payment"
+      ? (this.updatedBalance = this.runningBalance + amount)
+      : (this.updatedBalance = this.runningBalance - amount);
 
     // Update the running balance in the DB
-    this.currentFinancialDoc.ref.set({ [this.runningBalanceKey]: this.updatedBalance }, { merge: true })
-      .then(_ => { 
-        this.financialsService.runningBalanceForCurrentCategory$.next(this.updatedBalance); // For entry.component to show Running Balance
+    this.currentFinancialDoc.ref
+      .set({ [this.runningBalanceKey]: this.updatedBalance }, { merge: true })
+      .then(_ => {
+        this.financialsService.runningBalanceForCurrentCategory$.next(
+          this.updatedBalance
+        ); // For entry.component to show Running Balance
 
-        // Delete the subcollection that is the payment or charge being deleted.
+        // Delete the sub-collection that is the payment or charge being deleted.
         let collection: string;
-        // Determine subcollection.
-        type === 'Payment' ? collection = this.paymentsCollection : collection = this.chargesCollection;
-        this.currentFinancialDoc.ref.collection(collection).doc(id).delete()
+        // Determine sub-collection.
+        type === "Payment"
+          ? (collection = this.paymentsCollection)
+          : (collection = this.chargesCollection);
+        this.currentFinancialDoc.ref
+          .collection(collection)
+          .doc(id)
+          .delete()
           .then(_ => {
-            // console.log('TCL: HistoryComponent -> deleteTransaction -> id', id, 'Amount:', amount); 
             // Update history table data.
             this.setupHistory();
           });
-
       });
   }
 
@@ -151,5 +162,4 @@ export class HistoryComponent implements OnInit {
       sub.unsubscribe();
     });
   }
-
 }
