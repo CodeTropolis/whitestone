78238be7478968eaa4d98c12975ef8779339bd49
Michael <michael@codetropolis.com>
2019-03-01 15:10:55 -0600

    Prettier.

--- src/app/financials/student-category/student-category.component.ts
+++ src/app/financials/student-category/student-category.component.ts
@@ -1,92 +1,91 @@
-import { Component, OnInit } from '@angular/core';
-import { DataService } from '../../core/services/data.service';
-import { FinancialsService } from '../financials.service';
-import { BehaviorSubject } from 'rxjs';
-
+import { Component, OnInit } from "@angular/core";
+import { DataService } from "../../core/services/data.service";
+import { FinancialsService } from "../financials.service";
+import { BehaviorSubject } from "rxjs";
 
 @Component({
-  selector: 'app-student-category',
-  templateUrl: './student-category.component.html',
-  styleUrls: ['./student-category.component.css']
+  selector: "app-student-category",
+  templateUrl: "./student-category.component.html",
+  styleUrls: ["./student-category.component.css"]
 })
 export class StudentCategoryComponent implements OnInit {
-
   public currentRecord: any;
   public currentFinancialDoc: any;
   public studentsOfRecord: any[] = [];
-  public categories: any[] =[];
+  public categories: any[] = [];
   public currentStudent$: BehaviorSubject<any>;
   public enableCatButtons: boolean;
-  
+
   private subscriptions: any[] = [];
 
-  constructor(private dataService: DataService,  private financialsService: FinancialsService) { }
+  constructor(
+    private dataService: DataService,
+    private financialsService: FinancialsService
+  ) {}
 
   ngOnInit() {
-    // Set current financial doc to null else category buttons will be 
+    // Set current financial doc to null else category buttons will be
     //  active prior to selecting student which enables the buttons.
-    this.financialsService.currentFinancialDoc$.next(null); 
-    // Set current category to null else entry.component will 
+    this.financialsService.currentFinancialDoc$.next(null);
+    // Set current category to null else entry.component will
     //  detect a category and run through checks for balance and transactions.
     this.financialsService.currentCategory$.next(null);
 
     this.enableCatButtons = false;
     // currentRecord set by the 'more' menu on available records or Financial and Grades buttons, and soon to be Attendance button on non-admin UI.
     this.currentRecord = this.dataService.currentRecord;
-    if (this.currentRecord){
-      // Get all the children of the currentRecord. 
-     this.studentsOfRecord = this.dataService.convertMapToArray(this.currentRecord.children)
-   }else{
-     console.log('There is an issue obtaining the current record');
-   }
+    if (this.currentRecord) {
+      // Get all the children of the currentRecord.
+      this.studentsOfRecord = this.dataService.convertMapToArray(
+        this.currentRecord.children
+      );
+    } else {
+      console.log("There is an issue obtaining the current record");
+    }
 
-   // Listen for the currentFinancialDoc.
-   this.subscriptions.push(
-    this.financialsService.currentFinancialDoc$.subscribe(doc =>{ 
-      if(doc){
-        this.currentFinancialDoc = doc;
-				//console.log('TCL: StudentCategoryComponent -> ngOnInit -> his.currentFinancialDoc', this.currentFinancialDoc.data())
-        this.enableCatButtons = true;
-      }else {
-        this.enableCatButtons = false;
-        //console.log('financial doc null')
-      }
-    })
-  );
+    // Listen for the currentFinancialDoc.
+    this.subscriptions.push(
+      this.financialsService.currentFinancialDoc$.subscribe(doc => {
+        if (doc) {
+          this.currentFinancialDoc = doc;
+          //console.log('TCL: StudentCategoryComponent -> ngOnInit -> his.currentFinancialDoc', this.currentFinancialDoc.data())
+          this.enableCatButtons = true;
+        } else {
+          this.enableCatButtons = false;
+          //console.log('financial doc null')
+        }
+      })
+    );
 
     this.categories = this.financialsService.categories;
 
     // Set currentStudent to null so that we don't have a student set from previous use of this component.
-    this.financialsService.currentStudent$.next(null); 
-    // Listen for currentStudent selection 
+    this.financialsService.currentStudent$.next(null);
+    // Listen for currentStudent selection
     this.currentStudent$ = this.financialsService.currentStudent$;
-    // showHistory may still be set to true when entering back into this component.  
+    // showHistory may still be set to true when entering back into this component.
     //  Set to false to avoid null history.
-    this.financialsService.showHistory$.next(false); 
-
+    this.financialsService.showHistory$.next(false);
   }
 
-  public setFinancialDoc(student){ 
-
+  public setFinancialDoc(student) {
     // Will yield permission errors if non admin executes this.
     this.financialsService.setupFinancialDoc(student);
 
-     // Set currentCategory$ to prevent previously selected student's category entry form from showing
+    // Set currentCategory$ to prevent previously selected student's category entry form from showing
     this.financialsService.currentCategory$.next(null);
     this.financialsService.showHistory$.next(false); // Do not show history from previously selected student after clicking on another student
   }
 
-   public setCategory(cat){
+  public setCategory(cat) {
     this.financialsService.currentCategory$.next(cat);
 
-    setTimeout( _ =>{
+    setTimeout(_ => {
       window.scrollBy(0, 500);
-    }, 250)
-
+    }, 250);
   }
 
-  public goToURL(url){
-    window.open(url,'_blank');
+  public goToURL(url) {
+    window.open(url, "_blank");
   }
-
 }
