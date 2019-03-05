b314102b0313b8d59ecec163740986c959b31340
Michael <michael@codetropolis.com>
2019-03-05 12:18:39 -0600

    Fixed issuse where no records matching when a record matched.

--- src/app/record/record-list/record-list.component.ts
+++ src/app/record/record-list/record-list.component.ts
@@ -1,22 +1,20 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
-import { FirebaseService } from '../../core/services/firebase.service';
-import { RecordService } from '../record.service';
-import { MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
-import { DataService } from '../../core/services/data.service';
-import { AuthService } from '../../core/services/auth.service';;
-import { ModalService } from '../../modal/modal.service';
-import { AngularFirestore} from '@angular/fire/firestore';
-import { Observable, combineLatest } from 'rxjs'; // combineLatest works with this import only.
-import {  map } from 'rxjs/operators';
-
+import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
+import { FirebaseService } from "../../core/services/firebase.service";
+import { RecordService } from "../record.service";
+import { MatPaginator, MatSort, MatTableDataSource } from "@angular/material";
+import { DataService } from "../../core/services/data.service";
+import { AuthService } from "../../core/services/auth.service";
+import { ModalService } from "../../modal/modal.service";
+import { AngularFirestore } from "@angular/fire/firestore";
+import { Observable, combineLatest } from "rxjs"; // combineLatest works with this import only.
+import { map } from "rxjs/operators";
 
 @Component({
-  selector: 'app-record-list',
-  templateUrl: './record-list.component.html',
-  styleUrls: ['./record-list.component.css']
+  selector: "app-record-list",
+  templateUrl: "./record-list.component.html",
+  styleUrls: ["./record-list.component.css"]
 })
 export class RecordListComponent implements OnInit {
-
   public user: any;
   public records$: Observable<any[]>;
   public isUpdating: boolean;
@@ -25,29 +23,33 @@ export class RecordListComponent implements OnInit {
   public showChildren: boolean[] = [];
   public isDeleting: boolean[] = [];
   public showForm: boolean;
+  public recordMatch: boolean;
 
   // For modal
   public currentRecord: any;
   public modalTableDataSource: MatTableDataSource<any>;
-  public displayedColumnsModal = ['fatherEmail','motherEmail', 'address', 'catholic'];
+  public displayedColumnsModal = [
+    "fatherEmail",
+    "motherEmail",
+    "address",
+    "catholic"
+  ];
 
   private subscriptions: any[] = [];
 
-
   //@ViewChild(MatSort) sort: MatSort;
   @ViewChild(MatPaginator) paginator: MatPaginator;
-  
+
   // Prevent sort from undefined
   // https://stackoverflow.com/a/51084166
   private sort;
   @ViewChild(MatSort) set content(content: ElementRef) {
     this.sort = content;
-    if (this.sort){
-       this.ds.sort = this.sort;
-       this.ds.paginator = this.paginator;
+    if (this.sort) {
+      this.ds.sort = this.sort;
+      this.ds.paginator = this.paginator;
     }
   }
-
 
   constructor(
     private fs: FirebaseService,
@@ -56,19 +58,18 @@ export class RecordListComponent implements OnInit {
     private authService: AuthService,
     private modalService: ModalService,
     private afs: AngularFirestore
-  ) { }
+  ) {}
 
   ngOnInit() {
-
     this.ds = null;
 
     this.subscriptions.push(
-        this.authService.user$.subscribe(user =>{
-        if (user){
+      this.authService.user$.subscribe(user => {
+        if (user) {
           this.user = user; // Custom user object.
-          if (user['roles'].admin){
+          if (user["roles"].admin) {
             this.getAllRecords();
-          }else{
+          } else {
             this.getMatchingRecords();
           }
         }
@@ -76,57 +77,72 @@ export class RecordListComponent implements OnInit {
     );
 
     // Get state from service for button state: disable upon edit.
-    this.subscriptions.push(this.res.isUpdating$.subscribe(x => this.isUpdating = x));
+    this.subscriptions.push(
+      this.res.isUpdating$.subscribe(x => (this.isUpdating = x))
+    );
   }
 
-  private getAllRecords(){
+  private getAllRecords() {
     // this.displayedColumns = ['surname', 'fatherLname', 'motherLname', 'actions'];
-    this.displayedColumns = ['fatherLname', 'motherLname', 'actions'];
+    this.displayedColumns = ["fatherLname", "motherLname", "actions"];
 
     this.records$ = this.fs.records$;
 
     this.subscriptions.push(
       this.records$.subscribe(records => {
         this.ds = new MatTableDataSource(records);
-  
+
         // setTimeout(() => {
         //   this.ds.paginator = this.paginator;
         //   this.ds.sort = this.sort;
         // });
 
-
         this.ds.filterPredicate = (data, filter) => {
-          let dataStr = data.surname + data.fatherFname + data.fatherLname + data.motherFname + data.motherLname;
+          let dataStr =
+            data.surname +
+            data.fatherFname +
+            data.fatherLname +
+            data.motherFname +
+            data.motherLname;
           const children = this.dataService.convertMapToArray(data.children);
-          children.forEach(child => dataStr += (child.fname + child.lname + child.gender + child.grade + child.race));
+          children.forEach(
+            child =>
+              (dataStr +=
+                child.fname +
+                child.lname +
+                child.gender +
+                child.grade +
+                child.race)
+          );
           dataStr = dataStr.toLowerCase(); // MatTableDataSource defaults to lowercase matches
           return dataStr.indexOf(filter) != -1;
-        }
-
+        };
       })
     );
-    
   }
 
-  private getMatchingRecords(){
+  private getMatchingRecords() {
+    this.displayedColumns = ["surname", "actions"];
 
-    this.displayedColumns = ['surname', 'actions'];
+    const matchFatherEmail = this.afs.collection("records", ref => ref.where("fatherEmail", "==", this.user.email));
+    const matchMotherEmail = this.afs.collection("records", ref =>ref.where("motherEmail", "==", this.user.email));
 
-    const matchFatherEmail = this.afs.collection("records", ref => ref.where("fatherEmail","==", this.user.email));
-    const matchMotherEmail = this.afs.collection("records", ref => ref.where("motherEmail","==", this.user.email));
-
-    this.records$ = combineLatest(matchFatherEmail.valueChanges(), matchMotherEmail.valueChanges())
+    this.records$ = combineLatest(matchFatherEmail.valueChanges(),matchMotherEmail.valueChanges())
       .pipe(map(([fathers, mothers]) => {
-        return [...fathers, ...mothers];
-      }));
+          return [...fathers, ...mothers];
+        })
+      );
 
-      this.subscriptions.push(this.records$.subscribe(records =>{
+    this.subscriptions.push(
+      this.records$.subscribe(records => {
+        if(records){
         this.ds = new MatTableDataSource(records);
         this.ds.paginator = this.paginator;
         this.ds.sort = this.sort;
+        this.recordMatch = true;
+        }
       })
     );
-
   }
 
   applyFilter(filterValue: string) {
@@ -151,7 +167,7 @@ export class RecordListComponent implements OnInit {
     this.res.deleteRecord(record);
   }
 
-  public setCurrentRecord(record){
+  public setCurrentRecord(record) {
     this.dataService.setCurrentRecord(record);
     this.modalTableDataSource = new MatTableDataSource([record]); // data source must be an arrray.
   }
@@ -179,5 +195,4 @@ export class RecordListComponent implements OnInit {
       sub.unsubscribe();
     });
   }
-
 }
